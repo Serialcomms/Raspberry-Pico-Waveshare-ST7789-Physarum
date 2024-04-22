68d9e90a6c11ba4d1364901be1d250a24262762c
serialcomms <serialcomms@outlook.com>
2024-04-22 22:58:21 +0100

    Add files via upload

--- Physarum/pico_binary.c
+++ Physarum/pico_binary.c
@@ -3,11 +3,13 @@
 
 void set_binary_declarations() { // for use with Picotool
 
-    bi_decl(bi_program_description("Waveshare LCD"));
+    bi_decl(bi_program_description("Waveshare LCD - Physarum"));
     
     bi_decl(bi_program_feature("1.3 Inch LCD panel"));
     bi_decl(bi_program_feature("Joypad and buttons"));
+
     bi_decl(bi_program_version_string("Demo Version DMA/SPI1"));
+    
     bi_decl(bi_1pin_with_name(PIN_DC, "LCD Data/Command"));
     bi_decl(bi_1pin_with_name(PIN_CS, "LCD Chip Select"));
     bi_decl(bi_1pin_with_name(PIN_BL, "LCD Backlight"));
@@ -20,6 +22,5 @@ void set_binary_declarations() { // for use with Picotool
     bi_decl(bi_1pin_with_name(KEY_X, "Button X"));
     bi_decl(bi_1pin_with_name(KEY_Y, "Button Y"));
 
-    //bi_program_url("https://github.com/Serialcomms/Raspberry-Pico-Waveshare-ST7789-Physarum");
 }
 
